5645721edbf2bb0aa4d32b72fdcf9164ded0439a
QxQinaprO <118548170+QxQinaprO@users.noreply.github.com>
2022-11-19 12:11:42 +0800

    Update Exp-2.2.cpp

--- Exp-2.2.cpp
+++ Exp-2.2.cpp
@@ -1,10 +1,11 @@
 //Input a letter,output its uppercase if its a lowercase, while its a capital letter, output itself.
 #include<stdio.h>
-main()
+int main()
 {
 	printf("Input: ");
-    char a;
-    a=getchar(); 
-    if (a>='a'&&a<='z') printf("Its uppercase is %c",a-=32);
-    else printf("%c is a capital letter.",a);
+	char a;
+	a=getchar(); 
+	if (a>='a'&&a<='z') printf("Its uppercase is %c",a-=32);
+	else printf("%c is a capital letter.",a);
+	return 0;
 }
